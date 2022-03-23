c0dd6bcac2c3acf8773ef46ad1ba5ddda63c7320
Daniel Miguelez <zeleugim@gmail.com>
2022-03-23 12:45:41 +0100

    N1E2 OK

--- entrega13.js
+++ entrega13.js
@@ -4,7 +4,6 @@ Invoca-la passant-li les dues funcions de manera que imprimeixin un missatge dif
 de si la Promise es resol o no. */
 let promesa = new Promise  (function(resolve, reject){
     let x = Math.round(Math.random(0,2));
-    console.log(x);
     if (x==0){
         resolve();
     }else{    
@@ -19,3 +18,18 @@ promesa.then(
 Crea una arrow function que rebi un paràmetre i una funció callback 
 i li passi a la funció un missatge o un altre (que s'imprimirà per consola) 
 en funció del paràmetre rebut. */
+function callback (parametro){
+    let promesa = new Promise  (function(resolve, reject){
+        let x = Math.round(Math.random(0,2));
+        if (x==0){
+            resolve();
+        }else{    
+            reject();
+        }    
+    });
+}
+let parametro = Math.round(Math.random(0,2));
+let arrowFunc = (parametro, callback) => callback(parametro);
+/* Nivell 2 - Exercici 1
+Donats els objectes employees i salaries, crea una arrow function getEmployee() 
+que retorni una Promise efectuant la cerca en l'objecte pel seu id. */
