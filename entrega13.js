0cee23001689a7ed5f475656043216e6931d1d4e
Daniel Miguelez <zeleugim@gmail.com>
2022-03-23 11:54:20 +0100

    N1E1 OK

--- entrega13.js
+++ entrega13.js
@@ -1,1 +1,16 @@
-//Nivel 1 Exercici 1
+/* Nivel 1 Exercici 1
+Crea una funció que retorni una Promise que invoqui la funció resolve() o reject() que rep. 
+Invoca-la passant-li les dues funcions de manera que imprimeixin un missatge diferent depenent 
+de si la Promise es resol o no. */
+let promesa = new Promise  (function(resolve, reject){
+    let x = 0;
+    if (x==0){
+        resolve();
+    }else{    
+        reject();
+    }    
+});
+promesa.then(
+    function(value) {console.log("Bien")},
+    function(error) {console.log("Error")}
+); 
