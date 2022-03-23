3acffa924232d7604923f743c021939d07ff7926
Daniel Miguelez <zeleugim@gmail.com>
2022-03-23 12:09:15 +0100

    N1E1 OK

--- entrega13.js
+++ entrega13.js
@@ -1,9 +1,10 @@
-/* Nivel 1 Exercici 1
+/* Nivel 1 - Exercici 1
 Crea una funció que retorni una Promise que invoqui la funció resolve() o reject() que rep. 
 Invoca-la passant-li les dues funcions de manera que imprimeixin un missatge diferent depenent 
 de si la Promise es resol o no. */
 let promesa = new Promise  (function(resolve, reject){
-    let x = 0;
+    let x = Math.round(Math.random(0,2));
+    console.log(x);
     if (x==0){
         resolve();
     }else{    
@@ -14,3 +15,7 @@ promesa.then(
     function(value) {console.log("Bien")},
     function(error) {console.log("Error")}
 ); 
+/* Nivell 1 - Exercici 2
+Crea una arrow function que rebi un paràmetre i una funció callback 
+i li passi a la funció un missatge o un altre (que s'imprimirà per consola) 
+en funció del paràmetre rebut. */
