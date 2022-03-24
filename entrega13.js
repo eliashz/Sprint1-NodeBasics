564e2547f43b1b631c68f5a8bbae76c2400ea8e8
Daniel Miguelez <zeleugim@gmail.com>
2022-03-24 09:35:29 +0100

    Ahora sí terminado N1E2

--- entrega13.js
+++ entrega13.js
@@ -11,29 +11,33 @@ let promesa = new Promise  (function(resolve, reject){
     }    
 });
 promesa.then(
-    function(value) {console.log("Bien")},
-    function(error) {console.log("Error")}
+    function(value) {console.log("Bien (N1E1)")},
+    function(error) {console.log("Error (N1E1)")}
 ); 
 /* Nivell 1 - Exercici 2
 Crea una arrow function que rebi un paràmetre i una funció callback 
 i li passi a la funció un missatge o un altre (que s'imprimirà per consola) 
 en funció del paràmetre rebut. */
-function callback (parametro){
-    let promesa = new Promise  (function(resolve, reject){
-        let x = Math.round(Math.random(0,2));
-        if (x==0){
+function funcionCallback (parametro){
+    let promesa2 = new Promise  (function(resolve, reject){
+        if (parametro==0){
             resolve();
         }else{    
             reject();
         }    
     });
+    promesa2.then(
+        function(value) {console.log("Hola (N1E2")},
+        function(error) {console.log("Adiós (N1E2")}
+    );
 }
 let parametro = Math.round(Math.random(0,2));
-let arrowFunc = (parametro, callback) => callback(parametro);
+let arrowFuncion = (callback) => funcionCallback(parametro);
+arrowFuncion(parametro, ()=>{});
 /* Nivell 2 - Exercici 1
 Donats els objectes employees i salaries, crea una arrow function getEmployee() 
 que retorni una Promise efectuant la cerca en l'objecte pel seu id. */
-let employees = [{
+/* let employees = [{
     id: 1,
     name: 'Linux Torvalds'
 }, {
@@ -55,4 +59,4 @@ let salaries = [{
     salary: 2000
 }];
 let getEmployee = (id) => { };
-employees.
+employees. */
