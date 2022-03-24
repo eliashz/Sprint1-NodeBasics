40c3ef3059e9bc5c7246c50538d1168d5382ba1e
Daniel Miguelez <zeleugim@gmail.com>
2022-03-24 10:39:33 +0100

    N2E1

--- entrega13.js
+++ entrega13.js
@@ -19,25 +19,25 @@ Crea una arrow function que rebi un paràmetre i una funció callback
 i li passi a la funció un missatge o un altre (que s'imprimirà per consola) 
 en funció del paràmetre rebut. */
 function funcionCallback (parametro){
-    let promesa2 = new Promise  (function(resolve, reject){
+    let promesa = new Promise  (function(resolve, reject){
         if (parametro==0){
             resolve();
         }else{    
             reject();
         }    
     });
-    promesa2.then(
-        function(value) {console.log("Hola (N1E2")},
-        function(error) {console.log("Adiós (N1E2")}
-    );
+    promesa
+        .then (res => {console.log("Hola (N1E2)")})
+        .catch (err => {console.log("Adeu (N1E2)")});
 }
 let parametro = Math.round(Math.random(0,2));
-let arrowFuncion = (callback) => funcionCallback(parametro);
+let arrowFuncion = callback => funcionCallback(parametro);
 arrowFuncion(parametro, ()=>{});
 /* Nivell 2 - Exercici 1
 Donats els objectes employees i salaries, crea una arrow function getEmployee() 
 que retorni una Promise efectuant la cerca en l'objecte pel seu id. */
-/* let employees = [{
+let promesa21 = new Promise ( )
+let employees = [{
     id: 1,
     name: 'Linux Torvalds'
 }, {
@@ -58,5 +58,4 @@ let salaries = [{
     id: 3,
     salary: 2000
 }];
-let getEmployee = (id) => { };
-employees. */
+let getEmployee = () => {promesa21};
