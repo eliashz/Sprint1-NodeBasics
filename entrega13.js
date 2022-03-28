91783755d38a2cf77da839ea500afd77ab4c69d3
Daniel Miguelez <zeleugim@gmail.com>
2022-03-28 11:46:24 +0200

    Terminado

--- entrega13.js
+++ entrega13.js
@@ -2,18 +2,21 @@
 Crea una funció que retorni una Promise que invoqui la funció resolve() o reject() que rep. 
 Invoca-la passant-li les dues funcions de manera que imprimeixin un missatge diferent depenent 
 de si la Promise es resol o no. */
-let promesa = new Promise (function(resolve, reject){
-    let x = Math.round(Math.random(0,2));
-    if (x==0){
-        resolve();
-    }else{    
-        reject();
-    }    
-});
-promesa.then(
-    function(value) {console.log("Bien (N1E1)")},
-    function(error) {console.log("Error (N1E1)")}
-); 
+let funcionN1E1 = () => {
+    return new Promise (function(resolve, reject){
+        let x = Math.round(Math.random(0,2));
+        if (x==0){
+            resolve();
+        }else{    
+            reject();
+        }    
+    })
+    .then(
+        function(value) {console.log("Bien (N1E1)")},
+        function(error) {console.log("Error (N1E1)")}
+    ); 
+}
+funcionN1E1();
 /* Nivell 1 - Exercici 2
 Crea una arrow function que rebi un paràmetre i una funció callback 
 i li passi a la funció un missatge o un altre (que s'imprimirà per consola) 
