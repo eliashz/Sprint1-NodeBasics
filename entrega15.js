a94fc76767ea65daca52a3ff167cf2a43cabc16d
ehz8 <eliashz@gmail.com>
2022-03-30 17:47:17 +0200

    Falta final

--- entrega15.js
+++ entrega15.js
@@ -1,58 +1,97 @@
 /* Entrega 1.5: Node Utils
 
-Nivell 1 - Exercici 1
+Nivell 1 - Exercici 1 - COMENTADO!!!!*********************************
 Crea una funció que imprimeixi recursivament un missatge 
 per la consola amb demores d'un segon. */
-let n = -1000;
-while (n<10000){
-    setTimeout(() => {console.log('Imprimeixi recursivament un missatge');}, n+=1000);
-}
+/* let n = -500;
+(() => {while (n<5000){
+    setTimeout(() => {console.log('Imprimeixi recursivament un missatge');}, n+=500);
+}})(); */
 /* Nivell 1 - Exercici 2
 Crea una funció que, en executar-la, escrigui una frase en un fitxer. */
 const fs = require('fs');
-const archivo = 'prueba.txt';
+const archivo = 'ficheroN1E2.txt';
 const contenido = "Escrigui una frase en un fitxer."
-fs.writeFileSync(archivo, contenido);
+function crearFichero(archivo, contenido){
+    fs.writeFileSync(archivo, contenido);
+}
+crearFichero(archivo, contenido);
 /* Nivell 1 - Exercici 3
 Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior. */
-const texto = fs.readFileSync(archivo, {encoding:'utf8'});
-console.log("Texto del N1E3: "+texto);
+function mostrarFichero(archivo){
+    let texto = fs.readFileSync(archivo, {encoding:'utf8'});
+    console.log("Texto del N1E3: "+texto);
+    return texto
+}
+mostrarFichero(archivo);
 /* Nivell 2 - Exercici 1
 Crea una funció que comprimeixi el fitxer del nivell 1. */
 const archiver = require("archiver");
-var output = fs.createWriteStream('./prueba.zip');
-var archive = archiver('zip');
-archive.pipe(output);
-archive.append(fs.createReadStream('./prueba.txt'), {name: 'prueba.txt'});
-archive.finalize();
-/* Nivell 2 - Exercici 2
+function comprimir(archivo){
+    let output = fs.createWriteStream('./'+archivo);
+    let archive = archiver('zip');
+    archive.pipe(output);
+    archive.append(fs.createReadStream('./'+archivo), {name: archivo});
+    archive.finalize();
+}
+comprimir(archivo);
+/* Nivell 2 - Exercici 2 - COMENTADO!!!!*********************************
 Crea una funció que llisti per la consola el contingut del directori 
 d'usuari de l'ordinador utilizant Node Child Processes. */
-const exec = require('child_process').exec;
-exec('ls', () =>{
-    let file = fs.readdirSync("./");
-    console.log(file);
-}); 
+function mostrarDirectorio(){ 
+    let exec = require('child_process').exec;
+    exec('ls', () =>{
+        let file = fs.readdirSync("./");
+        console.log(file);
+    }); 
+}
+//mostrarDirectorio();
 /* Nivell 3 - Exercici 1
 Crea una funció que creï dos fitxers codificats en hexadecimal
 i en base64 respectivament, a partir del fitxer del nivell 1 */
 function codificacion(mensaje){
-    let archivoHex = 'archivoHex.txt';
-    let archivoBase64 = 'archivoBase64.txt';
     //Hexadecimal
-    let hex = '';
+    let ficheroHex = 'ficheroHex.txt';
+    let textoHex = '';
     let tempASCII, tempHex;
     mensaje.split('').map( i => {
         tempASCII = i.charCodeAt(0)
         tempHex = tempASCII.toString(16);
-        hex = hex + tempHex + ' ';
+        textoHex = textoHex + tempHex + ' ';
     });
-    hex = hex.trim();
-    fs.writeFileSync(archivoHex, hex);
+    textoHex = textoHex.trim();
+    crearFichero(ficheroHex, textoHex);
     //Base64
-    var base64 = btoa(texto);
-    fs.writeFileSync(archivoBase64, base64);
+    let ficheroBase64 = 'ficheroBase64.txt';
+    let textoBase64 = btoa(mensaje);
+    crearFichero(ficheroBase64, textoBase64);
 }
-codificacion(texto);
+codificacion(mostrarFichero(archivo));
 /* Crea una funció que guardi els fitxers del punt anterior, 
 ara encriptats amb l'algorisme aes-192-cbc, i esborri els fitxers inicials */
+let CryptoJS = require("crypto-js");
+let  encrypted;
+let key = '0123456789abcdef';
+let iv = '0123456789abcdef';
+function encriptar(texto){
+    key = CryptoJS.enc.Utf8.parse(key);
+    iv = CryptoJS.enc.Utf8.parse(iv);
+    encrypted = CryptoJS.AES.encrypt(texto, key, {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    encrypted = encrypted.toString();
+    crearFichero('ficheroAES.txt', encrypted);
+}
+encriptar(mostrarFichero(archivo));
+/* Crea una altra funció que desencripti i descodifiqui els fitxers de 
+l'apartat anterior tornant a generar una còpia de l'inicial */
+let decrypted = CryptoJS.AES.decrypt(encrypted, key, {
+    iv: iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7
+});
+decrypted = CryptoJS.enc.Utf8.stringify(decrypted);
+console.log(decrypted);
+crearFichero('copiaFicheroN1E2.txt', decrypted);
