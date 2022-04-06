faac0abc8c829416701e645c85564f15af65b862
Elías H <96485085+eliashz@users.noreply.github.com>
2022-04-06 19:31:26 +0200

    Entrega 1.4: Async / Await

--- entrega14.js
+++ entrega14.js
@@ -1,4 +1,5 @@
-/* Nivell 1 - Exercici 1
+/* Entrega 1.4: Async / Await
+Nivell 1 - Exercici 1
 Donats els objectes employees i salaries, crea una arrow
 function getEmployee que retorni una Promise efectuant la 
 cerca en l'objecte pel seu id. Crea una altra arrow function 
